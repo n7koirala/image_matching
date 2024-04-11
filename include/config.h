99e458b940ddf038d61ab9475acf98cbb4b070fc
Sam <smarti39@nd.edu>
2024-04-11 03:42:04 -0400

    formatted files with clang

--- include/config.h
+++ include/config.h
@@ -1,5 +1,6 @@
 // ** Holds configuration parameters like file paths, default values, and any other constants values.
 
+#include <string>
 #pragma once
 
 const std::string BACKEND_VECTORS_FILE = "input/backend_random.dat";
