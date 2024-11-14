2ba4a69db06c3166251712a3a282b92181797f49
smarti39 <smarti39@nd.edu>
2024-11-14 14:44:56 -0500

    bash script added for initializing experiment tracking .csv file

---
+++ tools/setup_experiment.sh
@@ -0,0 +1,12 @@
+FILEPATH="output/experiment.csv"
+
+# print .csv header for experiment file
+printf "Database Size," >> $FILEPATH
+printf "Query Encryption," >> $FILEPATH
+printf "Membership Computation," >> $FILEPATH
+printf "Membership Decryption," >> $FILEPATH
+printf "Index Computation," >> $FILEPATH
+printf "Index Decryption," >> $FILEPATH
+printf "Membership Result," >> $FILEPATH
+printf "Index Result" >> $FILEPATH
+printf "\n"  >> $FILEPATH
